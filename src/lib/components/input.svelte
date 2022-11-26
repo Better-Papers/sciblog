0839af1c76a5e74236d7ac170e189654efe749d1
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-11-26 11:39:46 -0500

    Update
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -6,18 +6,16 @@
   export let step = "";
 </script>
 
-<div class="">
-  <label class="origin-[0] scale-90 transform text-right text-sm text-gray-500 duration-300 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">
-    {label}
+<label class="origin-[0] scale-90 transform text-right text-gray-500 duration-300 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">
+  {label}
 
-    <input
-      type="number"
-      name="captureEff"
-      {min}
-      {max}
-      {step}
-      class="peer block border-0 border-b-2 border-gray-300 bg-transparent px-0 pb-1 pt-1.5 text-right text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
-      bind:value
-    />
-  </label>
-</div>
+  <input
+    type="number"
+    name="captureEff"
+    {min}
+    {max}
+    {step}
+    class="peer block border-0 border-b-2 border-gray-300 bg-transparent px-0 pb-1 pt-1.5 text-right text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+    bind:value
+  />
+</label>
