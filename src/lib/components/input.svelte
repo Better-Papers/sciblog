8c96824cb0223213a5af7a7421e7ec9e50867256
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-11-27 13:49:29 -0500

    Update
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -6,16 +6,15 @@
   export let step = "";
 </script>
 
-<label class="origin-[0] scale-90 transform text-right text-gray-500 duration-300 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">
+<label class="w-fit text-sm group origin-[0] text-right text-gray-600 flex flex-col items-end group-focus:font-medium group-focus:text-blue-600 dark:text-gray-400 group-focus:dark:text-blue-500">
   {label}
 
   <input
     type="number"
-    name="captureEff"
     {min}
     {max}
     {step}
-    class="peer block border-0 border-b-2 border-gray-300 bg-transparent px-0 pb-1 pt-1.5 text-right text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+    class="w-36 border-0 border-b-2 border-gray-300 bg-transparent transition-colors px-0 pb-1 pt-1.5 text-right text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
     bind:value
   />
 </label>
---
+++ src/lib/components/plotUtils.ts
@@ -0,0 +1,113 @@
+import * as d3 from "d3";
+
+export type FromPlot = {
+  figure: Node;
+  selection: d3.Selection<SVGSVGElement, unknown, HTMLElement, unknown>;
+  scales: Record<string, d3.AxisScale<[number, number]>>;
+  axes: Record<string, d3.Axis<[number, number]>>;
+  options: object;
+};
+
+export function updateYAxis({ selection, scales, axes }: FromPlot, transition = false) {
+  let sel = selection.select('[aria-label="y-axis"]');
+
+  if (transition) {
+    sel = sel.transition(50);
+  }
+
+  const yOpt = axes.y;
+  sel
+    .call(
+      d3
+        .axisLeft(scales.y)
+        .ticks(yOpt.ticks, yOpt.tickFormat)
+        .tickSizeInner(yOpt.tickSize)
+        .tickSizeOuter(0)
+        .tickPadding(yOpt.tickPadding)
+    )
+    .select(".domain")
+    .attr("stroke-width", 0);
+
+  // Add extended grid lines
+  selection
+    .select('[aria-label="y-axis"]')
+    .selectAll(".tick line:only-of-type")
+    .call((g) => g.clone(true).attr("stroke-opacity", 0.1).attr("x2", scales.x.range()[1]));
+}
+
+type NumString = number | string;
+
+export function updateLine(
+  fromPlot: FromPlot,
+  { X, Y, Z, I }: { X: number[]; Y: number[]; Z?: NumString[]; zDomain?: d3.InternSet<NumString>; I?: number[] },
+  transition = false
+) {
+  const line = d3
+    .line()
+    .curve(d3.curveBasis)
+    .x((i) => fromPlot.scales.x(X[i]))
+    .y((i) => fromPlot.scales.y(Y[i]));
+
+  if (Z?.includes(60000)) {
+    console.log(d3.group(I, (i) => Z[i]));
+  }
+
+  let sel = fromPlot.selection
+    .select('[aria-label = "line"]')
+    .selectAll("path")
+    .data(Z ? d3.group(I, (i) => Z[i]) : [[undefined, I]])
+    .join("path");
+
+  if (transition) {
+    sel = sel.transition(50);
+  }
+
+  sel.attr("d", ([, I]) => {
+    return line(I);
+  });
+}
+
+export function updateTextLegend(
+  { selection, scales }: FromPlot,
+  { X, Y, Z, I }: { X: number[]; Y: number[]; Z?: NumString; zDomain?: d3.InternSet<NumString>; I },
+  transition = false
+) {
+  let sel = selection
+    .select('[aria-label = "text"]')
+    .selectAll("text")
+    .data(d3.group(I, (i) => Z[i]))
+    .join("text");
+
+  if (transition) {
+    sel = sel.transition(50);
+  }
+
+  sel.attr("transform", (d) => `translate(${scales.x(X[d[1].at(-1)])},${scales.y(Y[d[1].at(-1)])})`);
+}
+
+export function update(fromPlot: FromPlot, data: object[], options: { x: string; y: string; z?: string }) {
+  const X = data.map((d) => d[options.x]);
+  const Y = data.map((d) => d[options.y]);
+  const Z = options.z ? data.map((d) => d[options.z]) : undefined;
+
+  const zDomain = Z ? new d3.InternSet(Z) : undefined;
+  const I = Z ? d3.range(X.length).filter((i) => zDomain!.has(Z[i])) : d3.range(X.length);
+
+  if (options.name) {
+    console.log(data);
+
+    console.log(data.map((d) => d["readsPerCell"]));
+    console.log(Z);
+  }
+
+  // Update Y-axis
+  fromPlot.scales.y.domain([d3.min(Y), d3.max(Y)]);
+  updateYAxis(fromPlot, true);
+
+  updateLine(fromPlot, { X, Y, Z, zDomain, I }, true);
+
+  const marks = fromPlot.options.marks.map((m) => m.constructor.name);
+  if (marks.includes("Text")) {
+    updateTextLegend(fromPlot, { X, Y, Z, zDomain, I }, true);
+  }
+}
