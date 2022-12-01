9bfc847dbd864568cfb5af54396e84464d9ed807
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-11-30 20:43:20 -0500

    Move get
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/routes/posts/index.json.ts
+++ src/routes/posts/index.json.ts
@@ -1,13 +1,16 @@
 import { posts } from "$src/lib/slug";
-import type { RequestHandler } from "./$types";
+import type { RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = ({ url }) => {
+export const get: RequestHandler = ({ url }) => {
   const limit = Number(url.searchParams.get("limit") ?? Infinity);
-  if (Number.isNaN(limit)) return new Response("", { status: 400 });
+  if (Number.isNaN(limit)) return { status: 400 };
 
   const tag = url.searchParams.get("tag");
   const filteredPosts = tag ? posts.filter((post) => post.post.metadata?.tags?.includes(tag)) : posts;
 
   // posts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
-  return new Response(JSON.stringify(filteredPosts.slice(0, limit)));
+
+  return {
+    body: filteredPosts.slice(0, limit),
+  };
 };
